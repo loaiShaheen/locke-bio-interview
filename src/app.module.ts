92ce865421c82ee9a069a323f31771327578cef7
Loai <loai.shaheen@gmail.com>
2021-08-12 16:19:33 -0400

    install prisma
    Installed prisma and added graphql to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      debug: true,
+      playground: true,
+    }),
+    ,
+  ],
+  providers: [PrismaService],
 })
 export class AppModule {}
